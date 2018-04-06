3a65dc7531d512a0d93e52b8e3f25fe9172e982b
johnlaschober <johnlaschober@gmail.com>
2018-04-06 09:41:33 -0500

    Added parsing from cache as backup

--- server.js
+++ server.js
@@ -4,25 +4,44 @@ var request = require('request'); // Imports libraries
 var fs = require('fs');
 
 var groupJSONs = [];
-
+var allURLs = ["http://gwb-json-info.azurewebsites.net/", "https://flamingos.azurewebsites.net/json"];
 var indexStack = [];
 
-function concatOtherGroupsJson(url)
-{
-	request(url, function (error, response, body) 
-	{
-		if (!error && response.statusCode == 200) 
-		{
-			var importedJSON = JSON.parse(body);
-			groupJSONs.push(importedJSON);
-		}
-	});
-}
+var combinedJSON;
 
 function concatGroupJsons()
 {
-	concatOtherGroupsJson("http://gwb-json-info.azurewebsites.net/");
-	concatOtherGroupsJson("https://flamingos.azurewebsites.net/json");
+	for (i = 0; i < allURLs.length; i++)
+	{
+		indexStack.push(i);
+		request(allURLs[i], function (error, response, body) 
+		{
+			if (!error && response.statusCode == 200) 
+			{
+				var importedJSON = JSON.parse(body);
+				groupJSONs.push(importedJSON);
+				indexStack.pop(i);
+				if (indexStack.length == 0)
+				{
+					try
+					{
+						combinedJSON = mergeJSON.merge(groupJSONs[0], groupJSONs[1]); // Need to manually add more merging here
+						try
+						{
+							fs.writeFile("classJSON.json", combinedJSON, 'utf-8', function (err)  // File writer for saving a json file, not done
+							{
+								if (err) 
+								{
+									return console.log("Error writing to classJSON.json: " + err);
+								}
+							});
+						} catch (err) {}
+						
+					} catch (err) {}
+				}
+			}
+		});
+	}
 	fs.writeFile("lastUpdated.txt", Date(), 'utf-8', function (err) 
 	{ 
 		if (err) 
@@ -47,17 +66,24 @@ var server = http.createServer(function (request, response)  // On user connect
 		try
 		{
 			var lastUpdated = fs.readFileSync('lastUpdated.txt', 'utf8');
-			console.log(lastUpdated);
+			console.log("Last updated: " + lastUpdated);
 		}
 		catch (err){}
-		response.end();
     }
     catch (err)
     {
-		response.writeHead(200, { "Content-Type": "text/plain" });
-        response.write("Something went wrong... " + err);
-		response.end();
+		try
+		{
+			var importedJSON = JSON.parse(fs.readFileSync('classJSON.json', 'utf8'));
+			response.write(JSON.stringify(importedJSON, null, 4));
+		}
+		catch (err) 
+		{
+			response.writeHead(200, { "Content-Type": "text/plain" });
+			response.write("Something went wrong... " + err);
+		}
     }
+	response.end();
 });
 
 var port = process.env.PORT || 80;
